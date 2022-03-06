9cf37d1b89ab62913b189ebb07b084b02749a1da
MRJAHID <MRJAHID350@GMAIL.COM>
2022-03-06 16:07:49 +0600

    Displaying a map marker

--- script.js
+++ script.js
@@ -16,12 +16,10 @@ if (navigator.geolocation) {
         function (position) {
             const { latitude } = position.coords;
             const { longitude } = position.coords;
-            console.log(latitude, longitude);
-            console.log(`https://www.google.com.bd/maps/@${latitude},${longitude},12z`);
 
             const map = L.map('map').setView([latitude, longitude], 13);
 
-            L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution:
                     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             }).addTo(map);
@@ -30,6 +28,23 @@ if (navigator.geolocation) {
                 .addTo(map)
                 .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
                 .openPopup();
+
+            map.on('click', function (mapEvent) {
+                const { lat, lng } = mapEvent.latlng;
+                L.marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(
+                        L.popup({
+                            maxWidth: 250,
+                            minWidth: 100,
+                            autoClose: false,
+                            closeOnClick: false,
+                            className: 'running-popup',
+                        })
+                    )
+                    .setPopupContent('Workout')
+                    .openPopup();
+            });
         },
         function () {
             alert('could not get your position');
